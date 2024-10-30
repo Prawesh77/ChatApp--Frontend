8676e392b5889d140cfac66e25e59c2a83c8a820
prawesh77 <praweshpoudel77@gmail.com>
2024-10-30 11:46:09 +0545

    feat(App): Route added based on login condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Chat from "./components/chat/Chat";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('accessToken'));
+
   return (
-    <>
-      Hello World
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/login"
+          element={!isLoggedIn ? <Login setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/chat" />}
+        />    
+        <Route
+          path="/chat"
+          element={isLoggedIn ? <Chat /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/"
+          element={<Navigate to={isLoggedIn ? "/chat" : "/login"} />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chat:React.FC = () => {
+  return (
+    <div className='flex flex-row h-100vh'>
+        Chat
+    </div>
+  )
+}
+
+export default Chat
